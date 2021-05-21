4b1aa4c89200170604cb5af585fe694ce6fb0f2d
mengjin <mengjin@jecn.com.cn>
2021-05-21 18:42:04 +0800

    [mengjin] N/A feat: 添加"webSocket"测试

--- src/containers/GridTest/index.js
+++ src/containers/GridTest/index.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
+import ConnectingWebSocket from '../../utils/ConnectingWebSocket';
+
 import PrListGrid from './PrListGrid';
 
 const Wrapper = styled.div`
@@ -10,9 +12,20 @@ const Wrapper = styled.div`
 `;
 
 class GridTest extends Component {
+  componentDidMount() {
+    this.socket = new ConnectingWebSocket('wss://echo.websocket.org', {
+      onOpen: () => {},
+    });
+  }
+
+  handleTest = () => {
+    this.socket.sendMessage('hello word');
+  };
+
   render() {
     return (
       <Wrapper>
+        <button onClick={this.handleTest}>Test</button>
         <PrListGrid currentTheme={'#4185f3'} />
       </Wrapper>
     );
